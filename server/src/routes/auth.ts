d075e570903aebfb9b15cc197f963765bba9110b
Anahlira <t.k.slavova@abv.bg>
2023-07-05 18:09:27 +0300

    Skeleton for auth

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,20 +1,21 @@
 import express from "express";
-const routerUsers = express.Router();
+const routerAuth = express.Router();
 import { getConnection, makeId } from "../../database/mongo";
 
 //get all users
-routerUsers.get("/", async (req, res) => {
+routerAuth.post("/login", async (req, res) => {
   const dbConn = getConnection();
   const dbTests = dbConn.collection("users");
 });
 
-routerUsers.get("/:id", async (req, res) => {
+routerAuth.post("/register", async (req, res) => {
   const dbConn = getConnection();
   const dbTests = dbConn.collection("users");
 });
 
-routerUsers.delete("/:id", async (req, res) => {
-  const dbConn = getConnection();
-  const dbTests = dbConn.collection("users");
+routerAuth.get("/logout", (req, res) => {
+  // req.session.destroy();
+  //   res.redirect("/");
 });
-export default routerUsers;
+
+export default routerAuth;
