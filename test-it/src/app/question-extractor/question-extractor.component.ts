6f4dddd6c58d112a589137c589974e971fcd71bb
Berna Ahad <bernaahad1@gmail.com>
2023-07-08 11:55:39 +0300

    Create question component from inputed text

--- test-it/src/app/question-extractor/question-extractor.component.ts
+++ test-it/src/app/question-extractor/question-extractor.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { IAnswer, IQuestion, ITest } from '../services/tests.service';
 
 @Component({
   selector: 'app-question-extractor',
@@ -11,6 +12,7 @@ import { AuthService } from '../services/auth.service';
 export class QuestionExtractorComponent {
   textInput: string = '';
   formBuilder = inject(FormBuilder);
+  createdTest: ITest | undefined = undefined;
 
   form = this.formBuilder.group({
     textInput: [this.textInput],
@@ -23,8 +25,8 @@ export class QuestionExtractorComponent {
       return;
     }
     const lines = this.form.value.textInput.split('\n');
-    let questions = [];
-    let currentQuestion = [];
+    let questions: string[][] = [];
+    let currentQuestion: string[] = [];
 
     for (let line of lines) {
       line = line.trim();
@@ -44,5 +46,50 @@ export class QuestionExtractorComponent {
     }
 
     console.log('Extracted Questions:', questions);
+    this.modifyToTest(questions);
+  }
+
+  modifyToTest(createdInitialQuestions: string[][]): void {
+    let questions: IQuestion[] = [];
+    let index = 0;
+    for (let question of createdInitialQuestions) {
+      if (question.length === 0) return;
+
+      const questionText = question.shift();
+
+      if (!questionText) return;
+      index++;
+
+      const answers: IAnswer[] = question.map((answer, answerIndex: number) => {
+        const answerTexts = answer.split('|');
+
+        if (answerTexts.length === 2) {
+          return {
+            id: answerIndex,
+            text: answerTexts[0].trim(),
+            correct:
+              answerTexts[1].trim().replaceAll(' ', '') === '1' ||
+              answerTexts[1].trim().replaceAll(' ', '') === '+1',
+          };
+        }
+
+        return { id: answerIndex, text: answer, correct: false };
+      });
+
+      console.log('answers', answers);
+      const correctAnswers = answers
+        .filter((answer) => answer.correct)
+        .map((answer) => answer.id);
+
+      questions.push({
+        questionText: question[0],
+        type: correctAnswers.length > 1 ? 'mc' : 'radio',
+        correctAnswers: [1],
+        id: index,
+        answers,
+      });
+    }
+    
+    console.log(questions);
   }
 }
